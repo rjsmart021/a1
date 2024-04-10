7704e08aee7f75ab38070f16ed43c0d90879db9b
rjsmart021 <rjsmart021@gmail.com>
2024-04-09 19:21:38 -0500

    firtst commit

---
+++ assighnment 1.py
@@ -0,0 +1,54 @@
+#Showcase your dance moves
+#Task 1 code correction
+weather = 'sunny'
+
+if weather == 'sunny':
+    print ("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+ 
+ #Task 2: Your Mood Today
+print("How do you feel today?")
+ans = 'sad'
+if ans == 'happy':
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
+#2.Python Naming Convention Practice
+#Task1:Code Correction
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+MAXLIMIT = 1000
+#3.Python Data Types and type() Function
+#Task 1: Code Correction
+variable_a = "Hello World!"
+print(type(variable_a))
+variable_b = 23
+print(type(variable_b))
+variable_c = 3.14
+print(type(variable_c))
+variable_d = True
+print(type(variable_d))
+#4.Arithetic Operations in Daily Life
+#Task 1:Grocery Store Math
+bread = 3
+Pickle = 2.50
+Jelly = 2
+print("Your total will be")
+my_sum = bread + Pickle + Jelly
+print(my_sum)
+#Task 2:Bank Interest
+Principle= 10000
+Interest= 8
+Time= 1
+Simple = (Principle*Interest*Time)/100
+print(Simple)
+#5.Exploring Logial Operations and Precedence
+#Task 1:Validating Calculations
+a=5
+b=6
+c=12
+my_t = a+b+c
+print(my_t)
+print(my_t/2)
